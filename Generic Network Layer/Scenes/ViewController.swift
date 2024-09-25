764010df08b62bfd37b933d004c1b326a2ebd9bc
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-09-25 13:40:47 +0300

    first commit

---
+++ Generic Network Layer/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Generic Network Layer
+//
+//  Created by Ahmet Tunahan Bekdaş on 25.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
+
---
+++ Generic Network Layer/Service/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Generic Network Layer
+//
+//  Created by Ahmet Tunahan Bekdaş on 25.09.2024.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    
+    
+}
